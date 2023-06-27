d894647cbf6b09ce907b23d85f92c8160db5a7a1
Michał K <karsaymichal@gmail.com>
2023-06-27 08:37:51 +0200

    font aded + hook for click outside el

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
+import { Arimo } from "next/font/google"
 
 export const metadata = {
   title: 'furnace',
   description: 'The forum for all your hot takes',
 }
 
+const arimo = Arimo({subsets: ['latin']})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className={cn(
+        'bg-white text-slate-900 antialiased light',
+        arimo.className
+      )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>{children}</body>
     </html>
   )
 }
