a5bbdcf251bed6d6ddc5dbb197d00bc2d7ba3686
Michał K <karsaymichal@gmail.com>
2023-09-20 17:00:38 +0200

    ui changes for create thread

--- src/app/f/create/page.tsx
+++ src/app/f/create/page.tsx
@@ -60,19 +60,19 @@ const Page =() => {
     return( 
     <div className='container flex items-center h-full max-w-3xl mx-auto'>
         <div className='relative bg-white w-full h-fit p-4 rounded-lg space-y-6'>
-            <div className='flex justify-between items-center'>
-                <h1 className='text-xl font-semibold'>Create a Flame</h1>
+            <div className='flex justify-between items-center '>
+                <h1 className='text-2xl font-semibold'>Create a <a className="text-orange-500">F</a>lame</h1>
             </div>
 
             <hr className='bg-zinc-500 h-px'/>
 
             <div>
-                <p className='text-lg font-medium'>Name</p>
-                <p className='text-xs pb-2'>
-                    Community names including captalization cannot be changed
+                <p className='text-xl font-medium'>Name</p>
+                <p className='text-sm pb-4'>
+                    Community names including capitalization cannot be changed
                 </p>
                 <div className='relative'>
-                    <p className='absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400'>
+                    <p className='absolute text-md left-0 w-8 inset-y-0 grid place-items-center text-orange-500'>
                         f/
                     </p>
                     <Input 
